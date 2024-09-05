f9172139867c42b7020f4998ec67e8a7cb8ddb90
triseed-billy <billy.bebit@triseed.com.ph>
2024-09-05 21:10:09 +0800

    first commit

---
+++ triseed/notes.js
@@ -0,0 +1,90 @@
+/**
+
+=== pipeline
+
+keytool -genkeypair -v -storetype PKCS12 -keystore pl-key.keystore -alias pl-alias -keyalg RSA -keysize 2048 -validity 10000
+passW0rd!
+
+billy ray
+triseed-engineering
+triseed
+silay
+neg occ
+ph
+
+MYAPP_UPLOAD_STORE_FILE=pl-key.keystore
+MYAPP_UPLOAD_KEY_ALIAS=pl-alias
+MYAPP_UPLOAD_STORE_PASSWORD=passW0rd!
+MYAPP_UPLOAD_KEY_PASSWORD=passW0rd!
+
+
+
+ === truck load
+
+EXPO_PUBLIC_API_URL=https://truck-validation-api.dev.porterpipe.com/api/v1/
+EXPO_PUBLIC_MS_LOGIN_URL=https://login.microsoftonline.com/09edc9fc-2632-4ede-b0ec-2bf1cd479f12/v2.0
+EXPO_PUBLIC_MS_CLIENT_ID=db978a10-c6a6-4526-8886-bb51a0d80979
+
+keytool -genkeypair -v -storetype PKCS12 -keystore tvl-key.keystore -alias tvl-alias -keyalg RSA -keysize 2048 -validity 10000
+t12uckP@55
+
+billy ray
+triseed-engineering
+triseed
+silay
+neg-occ
+ph
+
+MYAPP_UPLOAD_STORE_FILE=tvl-key.keystore
+MYAPP_UPLOAD_KEY_ALIAS=tvl-alias
+MYAPP_UPLOAD_STORE_PASSWORD=t12uckP@55
+MYAPP_UPLOAD_KEY_PASSWORD=t12uckP@55
+
+== previous
+
+MYAPP_UPLOAD_STORE_FILE=my-upload-key.keystore
+MYAPP_UPLOAD_KEY_ALIAS=my-key-alias
+MYAPP_UPLOAD_STORE_PASSWORD=davidjumola
+MYAPP_UPLOAD_KEY_PASSWORD=davidjumola
+
+=== env
+https://truck-load-api.dev.porterpipe.com/
+https://truck-load-api.prod.porterpipe.com/
+
+
+await WebBrowser.openAuthSessionAsync(process.env.EXPO_PUBLIC_MS_LOGOUT_URL);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
