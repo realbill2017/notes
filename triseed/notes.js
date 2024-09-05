492c396df54a81a8986df7966a5a2fdc6f90cb2e
triseed-billy <billy.bebit@triseed.com.ph>
2024-09-05 21:35:32 +0800

    env variables

--- triseed/notes.js
+++ triseed/notes.js
@@ -1,6 +1,6 @@
 /**
 
-=== pipeline
+// ==== PIPELINE ==== //
 
 keytool -genkeypair -v -storetype PKCS12 -keystore pl-key.keystore -alias pl-alias -keyalg RSA -keysize 2048 -validity 10000
 passW0rd!
@@ -12,17 +12,45 @@ silay
 neg occ
 ph
 
+
 MYAPP_UPLOAD_STORE_FILE=pl-key.keystore
 MYAPP_UPLOAD_KEY_ALIAS=pl-alias
 MYAPP_UPLOAD_STORE_PASSWORD=passW0rd!
 MYAPP_UPLOAD_KEY_PASSWORD=passW0rd!
 
 
+REACT_APP_BASEURL=https://api-pipeline.dev.porterpipe.com/
+REACT_APP_PORTAL_KEY=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyY2QyYzExNi02ZTJmLTQ3ZDYtODYxNi1lYjEzZjE4MWI1ZGIiLCJqdGkiOiJYUjRyYkRfMmJhdHM5M1VoVGxKbkQzMC13ME9LY1Rocm5vblpBamh4VlhZIn0.mlLBVTNKLw3RkzlmywW35bKZ7EKP4hKFdL2103zkwXtEi3hLiyxdBSFZkcYw2bpScsqu0wyFT39CSYe9XLgJSzpt0O9YdyOr95h5a_q0_DedVys7CC_3_soP0MHSQeX7VCN65KP0d9AuhoRG-tgANVAcbhsmocQk8Yobyki0ATjqXZdF-E5jEo0QggeXAUjZXXlzSC2KUsN5l0dZNrOyKMs1E6jYR-u78C_AGmcljaANqlqtY0E7h2sP8QhEY0Oe-fTFBUcaghu6RCV-bzb-dvy2Hk1eNzxpKHLPfb72-rD0qgPeAEt4F3x99I-TMUX9ARB6Y_1z51bBfANgq21Gpw
 
- === truck load
+=== WEB APP ===
+VITE_API_BASE_URL=https://api-pipeline.dev.porterpipe.com/api/v1
+
+VITE_MSAL_APP_ID=748ddeab-85d2-4d88-afdf-4cabba4049dc
+VITE_MSAL_AUTHORITY=https://login.microsoftonline.com/09edc9fc-2632-4ede-b0ec-2bf1cd479f12
+
+
+// === TRUCK LOAD === //
+ 
+# https://truck-load-api.dev.porterpipe.com/
+# https://truck-load-api.prod.porterpipe.com/
 
 EXPO_PUBLIC_API_URL=https://truck-validation-api.dev.porterpipe.com/api/v1/
 EXPO_PUBLIC_MS_LOGIN_URL=https://login.microsoftonline.com/09edc9fc-2632-4ede-b0ec-2bf1cd479f12/v2.0
+EXPO_PUBLIC_MS_CLIENT_ID=db978a10-c6a6-4526-8886-bb51a0d80979
+
+EXPO_PUBLIC_API_URL=https://truck-validation-api.dev.porterpipe.com/api/v1/
+EXPO_PUBLIC_MS_LOGIN_URL=https://login.microsoftonline.com/09edc9fc-2632-4ede-b0ec-2bf1cd479f12/v2.0
+EXPO_PUBLIC_MS_CLIENT_ID=db978a10-c6a6-4526-8886-bb51a0d80979
+
+
+# https://truck-load-api.dev.porterpipe.com/
+# https://truck-load-api.prod.porterpipe.com/
+# https://truck-validation-api.dev.porterpipe.com/api/v1/
+
+# EXPO_PUBLIC_API_URL=https://truck-load-api.dev.porterpipe.com/api/v1/
+EXPO_PUBLIC_API_URL=https://truck-load-api.prod.porterpipe.com/api/v1/
+EXPO_PUBLIC_MS_LOGIN_URL=https://login.microsoftonline.com/09edc9fc-2632-4ede-b0ec-2bf1cd479f12/v2.0
+EXPO_PUBLIC_MS_LOGOUT_URL=https://outlook.office.com/owa/logoff.owa
 EXPO_PUBLIC_MS_CLIENT_ID=db978a10-c6a6-4526-8886-bb51a0d80979
 
 keytool -genkeypair -v -storetype PKCS12 -keystore tvl-key.keystore -alias tvl-alias -keyalg RSA -keysize 2048 -validity 10000
@@ -53,6 +81,16 @@ https://truck-load-api.prod.porterpipe.com/
 
 
 await WebBrowser.openAuthSessionAsync(process.env.EXPO_PUBLIC_MS_LOGOUT_URL);
+
+// ==== MTR ==== //
+
+NODE_ENV=development
+
+EXPO_PUBLIC_API_BASE_URL=https://mtr-web-app-api.dev.porterpipe.com/api/v1
+
+EXPO_PUBLIC_MS_APP_ID=09edc9fc-2632-4ede-b0ec-2bf1cd479f12
+EXPO_PUBLIC_MS_TENANT_ID=7ae749f3-8014-44af-83d4-acd619ac01c0
+EXPO_PUBLIC_MS_LOGIN_URL=https://login.microsoftonline.com/09edc9fc-2632-4ede-b0ec-2bf1cd479f12/v2.0
 
 
 
